cae85cb0eeb49e215e4d4cbb01c8a024d8b8a6ca
Tirth Patel <tirthpatel.8695@gmail.com>
2024-01-17 00:00:50 -0500

    view created in xml for data to display

--- app/src/main/java/com/example/newsapplication/models/NewsApiResponse.java
+++ app/src/main/java/com/example/newsapplication/models/NewsApiResponse.java
@@ -1,27 +1,30 @@
 package com.example.newsapplication.models;
 
+import java.util.List;
+
 public class NewsApiResponse {
     String status;
-    String totalResults;
+    int totalResults;
+    List<NewsArticle> newsArticleList;
 
-    public NewsApiResponse(String status, String totalResults) {
-        this.status = status;
-        this.totalResults = totalResults;
+    public List<NewsArticle> getNewsArticleList() {
+        return newsArticleList;
     }
-
+    public void setNewsArticleList(List<NewsArticle> newsArticleList) {
+        this.newsArticleList = newsArticleList;
+    }
     public String getStatus() {
         return status;
     }
-
     public void setStatus(String status) {
         this.status = status;
     }
 
-    public String getTotalResults() {
+    public int getTotalResults() {
         return totalResults;
     }
 
-    public void setTotalResults(String totalResults) {
+    public void setTotalResults(int totalResults) {
         this.totalResults = totalResults;
     }
 }
